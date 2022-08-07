87ce28444718b0383fc7f57ca80b40250681a988
Kalanji <djiga2015@gmail.com>
2022-08-07 18:42:35 +0200

    my first dax

---
+++ src/main/java/com/tuthan/pharma_tech_back/PharmaTechBackApplication.java
@@ -0,0 +1,13 @@
+package com.tuthan.pharma_tech_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaTechBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaTechBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuthan/pharma_tech_back/PharmaTechBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuthan.pharma_tech_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaTechBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
